8f708b56c02507b89f44e1e4e54fdf8d82704fff
Mura Loïc <mura.loic0@gmail.com>
2015-12-31 13:22:06 +0100

    Add a pixelise function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <opencv2/opencv.hpp>
 
@@ -20,7 +21,6 @@ bool loadImage(Mat& img, string filename) {
 
 void convertImageToGrayScale(Mat& img) {
     cvtColor(img, img, COLOR_BGR2GRAY);
-    cout << "Image converted to gray scale" << endl;
 }
 /*
 int main(int argc, char** argv)
@@ -63,6 +63,66 @@ void reduceImage(Mat& img, int angle, double scale) {
     }
 }
 
+//No more imagination, sorry
+//Need the picture in gray scale
+void pixeliseImage(Mat& img, int electrodes_w, int electrodes_h) {
+    //To avoid errors
+    if(img.channels() != 1) {
+        cout << "Too more channels. Channel expected 1." << endl;
+        return;
+    }
+    if(electrodes_w == 0) {
+        electrodes_w = 1;
+    } else if(electrodes_w > img.cols) {
+        electrodes_w = img.cols;
+    }
+    if(electrodes_h == 0) {
+        electrodes_h = 1;
+    } else if(electrodes_h > img.rows) {
+        electrodes_h = img.rows;
+    }
+
+
+    Mat finalImg(electrodes_h, electrodes_w, CV_8UC1);
+    int blockW(img.cols / electrodes_w),
+        blockH(img.rows / electrodes_h);
+
+    //Need to cut the big picture in several small picture
+    for(int x(0); x < electrodes_w; ++x) {
+        for(int y(0); y < electrodes_h; ++y) {
+
+            //Now let's do the average of fray of the target part
+            Mat target(img, Rect(blockW * x, blockH * y, blockW, blockH));
+
+            int nRows(target.rows),
+                nCols(target.cols * target.channels()),
+                sum(0);
+
+            if(target.isContinuous()) {
+                nCols *= nRows;
+                nRows = 1;
+            }
+
+            uchar* p(nullptr);
+            for(int i(0); i < nRows; ++i) {
+                p = target.ptr<uchar>(i);
+                for(int j(0); j < nCols; ++j) {
+                    sum += p[j];
+                }
+            }
+
+            int average(sum / (blockH * blockW));
+
+            //Put this color in the target picture !
+            finalImg.ptr<uchar>(y)[x] = average;
+
+        }
+    }
+
+    img = finalImg;
+    //rectangle(img, Point(0, 0), Point(50, 50), Scalar(126), CV_FILLED);
+}
+
 int main() {
     VideoCapture webcam;
 
@@ -91,7 +151,6 @@ int main() {
     createTrackbar("angle (%)", paramsName, &angle, 100);
 
     bool carryOn(true);
-    bool mustConvert(false);
 
     while(carryOn) {
         //1 - Get picture
@@ -103,33 +162,21 @@ int main() {
                 carryOn = false;
                 break;
 
-            case 97:
-                mustConvert = !mustConvert;
-                break;
-
             default: break;
         }
 
         //2 - Convert to grayscale
-        if(mustConvert) {
-            convertImageToGrayScale(frame);
-        }
+        convertImageToGrayScale(frame);
 
-        uint8_t* pixelPtr = (uint8_t*)frame.data;
-        int cn = frame.channels();
-        Scalar_<uint8_t> bgrPixel;
-
-        bgrPixel.val[0] = pixelPtr[0];
-
-        //cout << frame.type() << endl;
-        //cout << (int)frame.at<unsigned int8_t>(Point(0, 0)) << " " << cvGet2D(frame., 0, 0) << endl;
-
-        //3 - Reduce
+        //3 - Reduce to get less information
         reduceImage(frame, angle, (double)electrodes_height / (double)electrodes_width);
 
+        //4 - Reduce against in electrodes_heigth * electrodes_width
+        pixeliseImage(frame, electrodes_height, electrodes_width);
 
+        //Show image
         imshow(windowName, frame);
-}
+    }
 
     return 0;
 }
